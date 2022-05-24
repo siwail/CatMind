b22e14ddf96c7e7ea1d0df2306fae31ca3f9a660
DinS1377 <102388196+DinS1377@users.noreply.github.com>
2022-05-24 16:42:39 +0300

    1

--- CatMind.py
+++ CatMind.py
@@ -1,3 +1,4 @@
+import pyttsx3
 import time
 cats = 1
 print("hi")
@@ -5,3 +6,6 @@ time.sleep(0.5)
 print("hi")
 time.sleep(0.5)
 print("hi")
+a = pyttsx3.speak("FUCK NIGGERS")
+
+
---
+++ test.py
@@ -0,0 +1,13 @@
+
+import pyttsx3
+cats = 10
+cat = []
+index = 0
+
+while index < cats:
+    cat.append('1')
+    index += 1
+
+pyttsx3.speak("FUCK NIGGERS")
+
+print(cat)
